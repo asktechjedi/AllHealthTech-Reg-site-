66b6db53d6b11e4a315b0bc50c3d9319f0432ae4
vishnutechjedi <vishnu@techjedi.in>
2026-05-12 17:03:56 +0530

    Read Google Sheets credentials from env variable

--- backend/src/services/googleSheetsService.js
+++ backend/src/services/googleSheetsService.js
@@ -31,7 +31,12 @@ export class PermanentSyncError extends Error {
  */
 async function getGoogleSheetsAuth(credentialsPath) {
   try {
-    const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
+    let credentials;
+    if (process.env.GOOGLE_SHEETS_CREDENTIALS_JSON) {
+      credentials = JSON.parse(process.env.GOOGLE_SHEETS_CREDENTIALS_JSON);
+    } else {
+      credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf8'));
+    }
     const auth = new google.auth.GoogleAuth({
       credentials,
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
