34ee57987b41482887fafc6cda7bc17108effc10
vishnutechjedi <vishnu@techjedi.in>
2026-06-12 16:29:13 +0530

    webmaster search console added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://allhealth.tech',
+      dynamicRoutes: [
+        '/about',
+        '/agenda',
+        '/speakers',
+        '/register',
+        '/contact',
+        '/policies',
+      ],
+    }),
+  ],
   server: {
     port: 5173,
     proxy: {
